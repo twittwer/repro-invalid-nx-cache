264bf1b10fe3c9ca0b0f12c7ba5be8769dc5d767
Tobias Wittwer <t.wittwer95@gmail.com>
2023-08-02 14:54:05 +0200

    Add & configure linting

--- apps/repro-invalid-nx-cache/src/app/app.component.ts
+++ apps/repro-invalid-nx-cache/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 @Component({
   standalone: true,
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
   selector: 'repro-invalid-nx-cache-root',
   template: `<h1>repro-invalid-nx-cache</h1>`,
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly _store = inject(Store)
+}
